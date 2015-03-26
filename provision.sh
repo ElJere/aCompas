08d9547325a9c41183f7d1f108bf33ce688dfa02
Olivier Ricordeau <olivier@codixy.com>
2015-03-26 04:23:51 +0100

    * Vagrant : use Ubuntu 14.10 Utopic Unicorn + automaticaly perform dist-upgrade + fixed box name + install VirtualBox guest addons inside the VM * bower.json : fixed keyword syntax

--- provision.sh
+++ provision.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:chris-lea/node.js
 apt-get update
-
-apt-get install -y -q curl vim git
-
-curl -sL https://deb.nodesource.com/setup | bash -
-
-apt-get install -y -q nodejs
-
+apt-get install -y -q curl vim git virtualbox-guest-dkms virtualbox-guest-utils nodejs
+apt-get dist-upgrade -u -q
 npm install -g bower
