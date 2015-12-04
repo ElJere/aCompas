a0f5e8a869d9166d24f513a74cf821b1b9571eea
Olivier Ricordeau <olivier@ricordeau.org>
2015-12-04 21:58:26 +0100

    Removed color classes from the templates and apply them in our scss file using SASS magics.

--- tests/jasmine/client/integration/homeSpec.js
+++ tests/jasmine/client/integration/homeSpec.js
@@ -1,7 +1,7 @@
 describe("Homepage", function() {
     it("Basic elements presence", function() {
         window.aCompas.testing.loadRoute("home");
-        var title = $("#main h2").html();
+        var title = $("main h2").html();
         expect(title).toBe("Home");
     });
 });
